938cb799e5bf638e9cbf23a7af6badcd11489a30
AndyLin <linxianfu928@qq.com>
2021-02-05 18:15:35 +0800

    静态文件浏览器缓存问题

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const { VERSION } = require('lodash');
 
 /*
  |--------------------------------------------------------------------------
@@ -13,4 +14,4 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+    .sourceMaps().version();
