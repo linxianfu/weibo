e022b1e1528cb69a5ef567fd2579ba350844448e
AndyLin <linxianfu928@qq.com>
2021-02-07 22:09:11 +0800

    查看用户列表

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use PhpParser\Node\Expr\AssignOp\Mod;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Model::unguard();
+        $this->call(UsersTableSeeder::class);
+        Model::reguard();
     }
 }
